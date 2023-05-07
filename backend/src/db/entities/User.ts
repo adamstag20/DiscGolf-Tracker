6d75b081f3ff8fa667062cf245c4d9f93f5f0e42
Adam Stagner <stagner2@pdx.edu>
2023-05-07 11:36:10 -0700

    Update to user table to better match my project

--- backend/src/db/entities/User.ts
+++ backend/src/db/entities/User.ts
@@ -1,6 +1,5 @@
 import { Entity, Property, Unique, OneToMany, Collection, Cascade } from "@mikro-orm/core";
 import { BaseEntity } from "./BaseEntity.js";
-import { Match } from "./Match.js";
 
 @Entity({ tableName: "users"})
 export class User extends BaseEntity {	
@@ -11,22 +10,4 @@ export class User extends BaseEntity {
 	@Property()
 	name!: string;
 	
-	@Property()
-	petType!: string;
-
-	// Note that these DO NOT EXIST in the database itself!
-	@OneToMany(
-		() => Match,
-		match => match.owner,
-		{cascade: [Cascade.PERSIST, Cascade.REMOVE]}
-	)
-	matches!: Collection<Match>;
-
-	@OneToMany(
-		() => Match,
-		match => match.matchee,
-		{cascade: [Cascade.PERSIST, Cascade.REMOVE]}
-	)
-	matched_by!: Collection<Match>;
-
-}
+};
--- backend/src/db/seeders/UserSeeder.ts
+++ backend/src/db/seeders/UserSeeder.ts
@@ -6,26 +6,22 @@ export class UserSeeder extends Seeder {
 	async run(em: EntityManager): Promise<void> {
 		em.create(User, {
 			name: "Spot",
-			email: "email@email.com",
-			petType: "Dog"
+			email: "email@email.com"
 		});
 
 		em.create(User, {
 			name: "Dogbert",
-			email: "email2@email.com",
-			petType: "Dog"
+			email: "email2@email.com"
 		});
 
 		em.create(User, {
 			name: "Doglord",
-			email: "email3@email.com",
-			petType: "Dog"
+			email: "email3@email.com"
 		});
 
 		em.create(User, {
 			name: "NotaDog",
-			email: "email4@email.com",
-			petType: "Cat"
+			email: "email4@email.com"
 		});
 	}
 }
