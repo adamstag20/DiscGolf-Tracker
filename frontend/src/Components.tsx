36a0908a4b120014338ba61c64bb3d56d786f9dc
Adam Stagner <stagner2@pdx.edu>
2023-05-12 16:41:37 -0700

    Working react-router-dom

---
+++ frontend/src/Components.tsx
@@ -0,0 +1,23 @@
+export const Match = () => {
+	return(
+		<div>"MATCH PAGE"</div>
+	);
+};
+
+export const Home = () => {
+	return (
+		<div>
+			<Title />
+			<Subtitle />
+
+		</div>
+	);
+};
+
+export function Title() {
+	return(<h1>Doggr</h1>);
+}
+
+export function Subtitle() {
+	return(<h3>Where your pets find love(tm)</h3>);
+}
