78987b3e25a1a8febfa69b2b12071fe384dfafa8
Adam Stagner <stagner2@pdx.edu>
2023-06-11 12:09:46 -0700

    Working docker compose up for frontend/backend woo!

--- frontend/src/Components.tsx
+++ frontend/src/Components.tsx
@@ -144,7 +144,7 @@ export const Home = () => {
 				{
 					courses.map((course: { course_name: string, difficulty: string }) =>
 						<div className="course" key={course.course_name}>
-							<Link clasName="course-link" to={`/start/${course.course_name}`}>
+							<Link className="course-link" to={`/start/${course.course_name}`}>
 								<h2> {course.course_name} </h2>
 								<h4> {course.difficulty} </h4>
 							</Link>
